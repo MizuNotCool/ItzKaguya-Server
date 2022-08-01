35a8fe4455d2516449ae37a0143c47543bdac491
ItzKaguya 2nd <106611054+MizuNotCool@users.noreply.github.com>
2022-08-01 13:36:51 +0800

    Update itzkaguya-stage1.sh

--- itzkaguya-stage1.sh
+++ itzkaguya-stage1.sh
@@ -10,5 +10,8 @@ cd termux-desktop-lxqt
 bash install-desktop-lxqt.sh
 cp start-desktop $HOME/start-desktop
 cp stop-desktop $HOME/stop-desktop
+pkg update && pkg upgrade -y
 wget -O $HOME/ubuntu.sh https://raw.githubusercontent.com/MizuNotCool/ItzKaguya-Server/main/ubuntu.sh
 proot-distro install ubuntu
+cd $HOME
+bash ubuntu.sh
