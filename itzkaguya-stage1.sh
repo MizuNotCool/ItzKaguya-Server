630649368f7ba097cfacb397dd1e27873a73ff4f
ItzKaguya 2nd <106611054+MizuNotCool@users.noreply.github.com>
2022-08-01 13:24:19 +0800

    Create itzkaguya-stage1.sh

---
+++ itzkaguya-stage1.sh
@@ -0,0 +1,14 @@
+echo "Script Desktop Environment Installer"
+echo "a little project by I Putu Arya Sukanadi (aka ItzKaguya)"
+sleep 3
+echo "Setting up environment..."
+cd $HOME
+pkg update && pkg upgrade -y
+pkg install git proot-distro htop bmon neofetch wget -y
+git clone --depth 1 https://github.com/Yisus7u7/termux-desktop-lxqt 
+cd termux-desktop-lxqt 
+bash install-desktop-lxqt.sh
+cp start-desktop $HOME/start-desktop
+cp stop-desktop $HOME/stop-desktop
+wget -O $HOME/ubuntu.sh https://raw.githubusercontent.com/MizuNotCool/ItzKaguya-Server/main/ubuntu.sh
+proot-distro install ubuntu
